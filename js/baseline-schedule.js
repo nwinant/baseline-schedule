58524eb6e13ba432460d5b899b351f446d3b58e4
Nathan Winant <nw@exegetic.net>
2014-10-20 10:00:07 -0500

    Added BaselineUtils namespace for misc functions; will want to reorganize further.

--- js/baseline-schedule.js
+++ js/baseline-schedule.js
@@ -1,10 +1,13 @@
-
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+// TODO: we'll probably want to change the namespacing here...
+var BaselineUtils = {
+    getParameterByName: function (name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
 }
+
 /*
 function Stopwatch(milliseconds) {
     var startDate;
